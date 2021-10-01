a3f142407d3449d0d72ea4b94d5b3ecb110848fd
Gergely Jakab <tca9996@gmail.com>
2021-10-01 17:17:42 +0200

    update

--- assets.py
+++ assets.py
@@ -1,6 +1,10 @@
+import datetime
+
 import gspread
 import json
+import time
 
+from sty import *
 from googleapiclient import discovery
 from enumerator_dickts import *
 from datetime import date
@@ -21,10 +25,12 @@ def open_spread_sheet(spreadsheet_name: str):
     return spreadsheet, creds
 
 
-def update_sheet_list(spreadsheet):
+def update_sheet_list(spreadsheet_name):
+    spreadsheet, credentials = open_spread_sheet(spreadsheet_name)
+
     ssheet = {}
 
-    print('Updating sheet list...')
+    print(ef.italic + ef.bold + fg.yellow + 'Updating sheet list, please wait...' + ef.rs + fg.rs)
 
     l = len(spreadsheet.worksheets())
     k = 0
@@ -37,14 +43,14 @@ def update_sheet_list(spreadsheet):
         percent = (i/l)*100
 
         if k == 9:
-            print(f'done: {round(percent)}%')
+            print(fg.yellow +  f'done: {round(percent)}%' + fg.rs)
             k = 0
         k += 1
 
     with open('sheets.jason', 'w') as file:
         json.dump(ssheet, file)
 
-    print(f'done: 100%')
+    print(ef.italic + ef.bold + fg.yellow + f'done: 100%' + ef.rs + fg.rs)
 
 
 def load_json(json_file):
@@ -58,10 +64,11 @@ def find_sheet_by_name(name: str, sheet_dict: dict):
     sheet = []
 
     for key in sheet_dict:
-        if key == name:
+        if str(key) == name:
             sheet.append(name)
             sheet.append(sheet_dict[key][0])
             sheet.append(sheet_dict[key][1])
+            break
 
     return sheet
 
@@ -71,7 +78,7 @@ def calc_date():
     date_dict = {}
 
     if today.month == 1:
-        date_dict = {'prev_month': f'{today.year - 1}.0{today.month + 11}.',
+        date_dict = {'prev_month': f'{today.year - 1}.{today.month + 11}.',
                      'this_month': f'{today.year}.0{today.month}.',
                      'next_month': f'{today.year}.0{today.month + 1}.'}
     elif today.month < 9:
@@ -98,60 +105,7 @@ def calc_date():
     return date_dict
 
 
-def add_new_month(wbook, creds, is_new_month=False):
-    dates = calc_date()
-    template_sheet = wbook.worksheet('Template')
-    is_new_year = False
-
-    if is_new_month:
-        new_sheet = wbook.duplicate_sheet(source_sheet_id=template_sheet.id,
-                                          insert_sheet_index=11,
-                                          new_sheet_name=dates['this_month'])
-
-        cell = new_sheet.acell('A3', 'FORMULA')
-        new_value = cell.value[:2] + dates['prev_month'] + cell.value[-4:]
-        new_sheet.update('A3', new_value, raw=False)
-
-        for i in range(13):
-            new_sheet.update(f'E{i+3}', [[f"{dates['this_month']}01."]], raw=False)
-
-        past_year = int(dates['prev_month'][:-4])
-        year = int(dates['this_month'][:-4])
-
-        if past_year < year:
-            is_new_year = True
-
-    else:
-        new_sheet = wbook.duplicate_sheet(source_sheet_id=template_sheet.id,
-                                          insert_sheet_index=11,
-                                          new_sheet_name=dates['next_month'])
-
-        cell = new_sheet.acell('A3', 'FORMULA')
-        new_value = cell.value[:2] + dates['this_month'] + cell.value[-4:]
-        new_sheet.update('A3', new_value, raw=False)
-
-        for i in range(13):
-            new_sheet.update(f'E{i+3}', [[f"{dates['next_month']}01."]], raw=False)
-
-        year = int(dates['this_month'][:-4])
-        next_year = int(dates['next_month'][:-4])
-
-        if year < next_year:
-            is_new_year = True
-
-    print('New sheet added succesfully!')
-
-    show_hide_wsheets(creds=creds, wbook_id=wbook.id, sheet_id=new_sheet.id, is_hidden=False)
-
-    if is_new_year:
-        add_new_year(wbook=wbook, creds=creds, is_new_month=is_new_month)
-
-    edit_mounthly_costs_sheet(wbook=wbook, creds=creds, is_new_month=is_new_month, dates=dates)
-
-
 def edit_mounthly_costs_sheet(wbook, creds, is_new_month=False, dates=calc_date()):
-    year = None
-
     if is_new_month:
         year = dates['this_month'][:-4]
     else:
@@ -169,9 +123,9 @@ def edit_mounthly_costs_sheet(wbook, creds, is_new_month=False, dates=calc_date(
             col = None
             key_value = dates['this_month'][-3:]
 
-            for key in mounthly_costs_columns:
+            for key in mounthly_columns:
                 if key == key_value:
-                    col = mounthly_costs_columns[key]
+                    col = mounthly_columns[key]
                     cost_sheet.update(f'{col[0]}{i + 2}', [value], raw=False)
 
         show_hide_cols(creds=creds, wbook_id=wbook.id, sheet_id=cost_sheet.id, is_hidden=False, start=col[1]-1, end=col[1])
@@ -182,12 +136,53 @@ def edit_mounthly_costs_sheet(wbook, creds, is_new_month=False, dates=calc_date(
             col = None
             key_value = dates['next_month'][-3:]
 
-            for key in mounthly_costs_columns:
+            for key in mounthly_columns:
                 if key == key_value:
-                    col = mounthly_costs_columns[key]
+                    col = mounthly_columns[key]
                     cost_sheet.update(f'{col[0]}{j + 2}', [value], raw=False)
 
         show_hide_cols(creds=creds, wbook_id=wbook.id, sheet_id=cost_sheet.id, is_hidden=False, start=col[1] - 1, end=col[1])
+
+    print('Done!')
+
+
+def edit_savings_sheet(wbook, creds, is_new_month=False, dates=calc_date()):
+    if is_new_month:
+        year = dates['this_month'][:-4]
+    else:
+        year = dates['next_month'][:-4]
+
+    sheet_name = f'Megtakarítás részletező {year}'
+    saving_sheet = wbook.worksheet(sheet_name)
+    count = saving_sheet.acell('A1').numeric_value
+
+    print(f'Modifying savings table ({saving_sheet.title})  ...', end='  ')
+
+    if is_new_month:
+        for i in range(count):
+            value = [f"=SUMIF('{dates['this_month']}'!$D$4:$D$100;'{sheet_name}'!$A{i + 2};'{dates['this_month']}'!$A$4:$A$100)*-1"]
+            col = None
+            key_value = dates['this_month'][-3:]
+
+            for key in mounthly_columns:
+                if key == key_value:
+                    col = mounthly_columns[key]
+                    saving_sheet.update(f'{col[0]}{i + 2}', [value], raw=False)
+
+        show_hide_cols(creds=creds, wbook_id=wbook.id, sheet_id=saving_sheet.id, is_hidden=False, start=col[1]-1, end=col[1])
+
+    else:
+        for j in range(count):
+            value = [f"=SUMIF('{dates['next_month']}'!$D$4:$D$100;'{sheet_name}'!$A{j + 2};'{dates['next_month']}'!$A$4:$A$100)*-1"]
+            col = None
+            key_value = dates['next_month'][-3:]
+
+            for key in mounthly_columns:
+                if key == key_value:
+                    col = mounthly_columns[key]
+                    saving_sheet.update(f'{col[0]}{j + 2}', [value], raw=False)
+
+        show_hide_cols(creds=creds, wbook_id=wbook.id, sheet_id=saving_sheet.id, is_hidden=False, start=col[1] - 1, end=col[1])
 
     print('Done!')
 
@@ -230,9 +225,120 @@ def show_hide_wsheets(creds, wbook_id, sheet_id, is_hidden):
     return response
 
 
+def add_new_month(wbook, creds, is_new_month=False):
+    dates = calc_date()
+    template_sheet = wbook.worksheet('Template')
+    is_new_year = False
+
+    if is_new_month:
+        prev_sheet = wbook.worksheet(dates['prev_month'])
+        dict = find_sheet_by_name(dates['prev_month'], load_json('sheets.jason'))
+
+        new_sheet = wbook.duplicate_sheet(source_sheet_id=template_sheet.id,
+                                          insert_sheet_index=dict[2]-1,
+                                          new_sheet_name=dates['this_month'])
+
+        cell = new_sheet.acell('A3', 'FORMULA')
+        new_value = cell.value[:2] + dates['prev_month'] + cell.value[-4:]
+        new_sheet.update('A3', new_value, raw=False)
+
+        cell2 = new_sheet.acell('H2', 'FORMULA')
+        new_value2 = cell2.value[:31] + dates['this_month'][:-4] + cell2.value[-5:]
+        new_sheet.update('H2', new_value2, raw=False)
+
+        for i in range(13):
+            new_sheet.update(f'E{i+3}', [[f"{dates['this_month']}01."]], raw=False)
+
+        past_year = int(dates['prev_month'][:-4])
+        year = int(dates['this_month'][:-4])
+
+        if past_year < year:
+            is_new_year = True
+
+        show_hide_wsheets(creds=creds, wbook_id=wbook.id, sheet_id=prev_sheet.id, is_hidden=True)
+
+    else:
+        prev_sheet = wbook.worksheet(dates['prev_month'])
+        dict = find_sheet_by_name(dates['this_month'], load_json('sheets.jason'))
+
+        new_sheet = wbook.duplicate_sheet(source_sheet_id=template_sheet.id,
+                                          insert_sheet_index=dict[2]-1,
+                                          new_sheet_name=dates['next_month'])
+
+        cell = new_sheet.acell('A3', 'FORMULA')
+        new_value = cell.value[:2] + dates['this_month'] + cell.value[-4:]
+        new_sheet.update('A3', new_value, raw=False)
+
+        cell2 = new_sheet.acell('H2', 'FORMULA')
+        new_value2 = cell2.value[:31] + dates['next_month'][:-4] + cell2.value[-5:]
+        new_sheet.update('H2', new_value2, raw=False)
+
+        for i in range(13):
+            new_sheet.update(f'E{i+3}', [[f"{dates['next_month']}01."]], raw=False)
+
+        year = int(dates['this_month'][:-4])
+        next_year = int(dates['next_month'][:-4])
+
+        if year < next_year:
+            is_new_year = True
+
+    print('New sheet added succesfully!')
+
+    show_hide_wsheets(creds=creds, wbook_id=wbook.id, sheet_id=new_sheet.id, is_hidden=False)
+
+    edit_savings_sheet(wbook=wbook, creds=creds, is_new_month=is_new_month, dates=dates)
+    edit_mounthly_costs_sheet(wbook=wbook, creds=creds, is_new_month=is_new_month, dates=dates)
+
+    if is_new_year:
+        add_new_year(wbook=wbook, creds=creds, is_new_month=is_new_month)
+
+
 def add_new_year(wbook, creds, is_new_month):
-    # TODO impelent new year
-    print('Not implemented yet')
+    dates = calc_date()
+    template_sheet_1 = wbook.worksheet('Megtakarítás részletező template')
+    template_sheet_2 = wbook.worksheet('Havi Kiadások Template')
+
+    if is_new_month:
+        prev_sheet_1 = wbook.worksheet(f"Megtakarítás részletező {dates['prev_month'][:-4]}")
+        prev_sheet_2 = wbook.worksheet(f"Havi Kiadások {dates['prev_month'][:-4]}")
+
+        dict = find_sheet_by_name(dates['prev_month'], load_json('sheets.jason'))
+
+        new_sheet_1 = wbook.duplicate_sheet(source_sheet_id=template_sheet_1.id,
+                                          insert_sheet_index=dict[2],
+                                          new_sheet_name=f"Megtakarítás részletező {dates['this_month'][:-4]}")
+
+        new_sheet_2 = wbook.duplicate_sheet(source_sheet_id=template_sheet_2.id,
+                                            insert_sheet_index=dict[2],
+                                            new_sheet_name=f"Havi Kiadások {dates['this_month'][:-4]}")
+
+        edit_savings_sheet(wbook=wbook, creds=creds, is_new_month=is_new_month, dates=dates)
+        edit_mounthly_costs_sheet(wbook=wbook, creds=creds, is_new_month=is_new_month, dates=dates)
+
+    else:
+        prev_sheet_1 = wbook.worksheet(f"Megtakarítás részletező {dates['this_month'][:-4]}")
+        prev_sheet_2 = wbook.worksheet(f"Havi Kiadások {dates['this_month'][:-4]}")
+
+        dict = find_sheet_by_name(dates['this_month'], load_json('sheets.jason'))
+
+        new_sheet_1 = wbook.duplicate_sheet(source_sheet_id=template_sheet_1.id,
+                                            insert_sheet_index=dict[2] - 1,
+                                            new_sheet_name=f"Megtakarítás részletező {dates['next_month'][:-4]}")
+
+        new_sheet_2 = wbook.duplicate_sheet(source_sheet_id=template_sheet_2.id,
+                                            insert_sheet_index=dict[2] - 1,
+                                            new_sheet_name=f"Megtakarítás részletező {dates['next_month'][:-4]}")
+
+        edit_savings_sheet(wbook=wbook, creds=creds, is_new_month=is_new_month, dates=dates)
+        edit_mounthly_costs_sheet(wbook=wbook, creds=creds, is_new_month=is_new_month, dates=dates)
+
+    print('New year added succesfully!')
+
+    show_hide_wsheets(creds=creds, wbook_id=wbook.id, sheet_id=new_sheet_1.id, is_hidden=False)
+    show_hide_wsheets(creds=creds, wbook_id=wbook.id, sheet_id=new_sheet_2.id, is_hidden=False)
+    show_hide_wsheets(creds=creds, wbook_id=wbook.id, sheet_id=prev_sheet_1.id, is_hidden=True)
+    show_hide_wsheets(creds=creds, wbook_id=wbook.id, sheet_id=prev_sheet_2.id, is_hidden=True)
+
 
 def add_new_category():
     # TODO impelent new category
@@ -241,6 +347,12 @@ def add_new_category():
 
 if __name__ == '__main__':
     workb, creds = open_spread_sheet('Pénz másolata')
-    date = calc_date()
 
-    edit_mounthly_costs_sheet(wbook=workb, creds=creds, is_new_month=True, dates=date)
+    # update_sheet_list('Pénz másolata')
+    add_new_month(wbook=workb, creds=creds, is_new_month=True)
+    # calc_date()
+
+    # sheet = workb.worksheet('2021.12.')
+    # cell = sheet.acell('H2', 'FORMULA')
+
+    print('gg')
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from assets import *
 
 def choose_from_menu():
     while True:
-        x = input('Choose a number from above (1, 2 or 3): ')
+        x = input(fg.li_green + 'Choose a number from above: ' + fg.rs)
         if x in menu:
             break
 
@@ -13,7 +13,7 @@ def is_new_month():
     truth = None
 
     while True:
-        value = input('Is today is in the new month/year you want to add? (y/n) ')
+        value = input(fg.li_green + 'Is today in the new month/year you want to add? (y/n) ' + fg.rs)
         if value in yn:
             break
 
@@ -25,30 +25,53 @@ def is_new_month():
     return truth
 
 def main_menu(x):
-    workb, creds = open_spread_sheet('Pénz másolata')
+    workb, creds = open_spread_sheet(sheetname)
 
     if x == '1':
         truth = is_new_month()
         add_new_month(wbook=workb, creds=creds, is_new_month=truth)
 
-    elif x == '3':
+    elif x == '2':
         add_new_category()
+
+    elif x == '3':
+        dates = calc_date()
+        truth = is_new_month()
+        edit_mounthly_costs_sheet(wbook=workb, creds=creds, is_new_month=truth, dates=dates)
+
+    elif x == '4':
+        dates = calc_date()
+        truth = is_new_month()
+        edit_savings_sheet(wbook=workb, creds=creds, is_new_month=truth, dates=dates)
+
+    elif x == '5':
+        update_sheet_list(sheetname)
 
 
 if __name__ == '__main__':
-    menu = ['1', '2']
+    sheetname = 'Pénz'
+    # sheetname = 'Pénz másolata'
+
+    menu = ['1', '2', '3', '4', '5']
     yn = ['y', 'n']
 
-    print('~'*25, ' Welcome to Walet editor JohnSlow! ', '~'*25)
+    print(ef.bold + fg.cyan + '~'*25, ' Welcome to Walet editor JohnSlow! ', '~'*25 + fg.rs + ef.rs)
     print('What would you like to do?')
     print('  1 - Add new month')
     print('  2 - Add new category')
+    print('  3 - Refresh costs table')
+    print('  4 - Refresh savings table')
+    print('  5 - Refresh sheet list')
+    print(ef.italic + ef.bold + fg.yellow + '- If you want to add a new month, but the program sopped during\n'
+                                            '  the procedure, please re run the program and choose 3 and than 4.' + ef.rs + fg.rs)
+    print(ef.italic + ef.bold + fg.yellow + '- Choose 5, if the program crases, and there is a "list index out of range"\n'
+                                            '  line in the error message.\n' + ef.rs + fg.rs)
 
     choose_from_menu()
 
     while True:
         while True:
-            y = str(input('Do you want to do something else? (y/n) '))
+            y = str(input(fg.li_green + 'Do you want to do something else? (y/n) ' + fg.rs))
             if y in yn:
                 break
 
@@ -56,5 +79,8 @@ if __name__ == '__main__':
             choose_from_menu()
 
         elif y == 'n':
-            print('Good bye!')
+
+            update_sheet_list(sheetname)
+
+            print(fg.cyan + 'Good bye!' + fg.rs)
             break
