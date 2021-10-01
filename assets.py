496fdce79a4eaa71bc2925a2062cad160450be1f
Gergely Jakab <gergely.jakab@aimotive.com>
2021-10-01 10:51:01 +0200

    update

--- assets.py
+++ assets.py
@@ -1,7 +1,7 @@
 import gspread
 import json
-import pandas
 
+from googleapiclient import discovery
 from enumerator_dickts import *
 from datetime import date
 from oauth2client.service_account import ServiceAccountCredentials
@@ -18,7 +18,7 @@ def open_spread_sheet(spreadsheet_name: str):
     spreadsheet = client.open(spreadsheet_name)
     print(f'Spreadsheet opened: {spreadsheet.title}')
 
-    return spreadsheet
+    return spreadsheet, creds
 
 
 def update_sheet_list(spreadsheet):
@@ -68,6 +68,7 @@ def find_sheet_by_name(name: str, sheet_dict: dict):
 
 def calc_date():
     today = date.today()
+    date_dict = {}
 
     if today.month == 1:
         date_dict = {'prev_month': f'{today.year - 1}.0{today.month + 11}.',
@@ -96,45 +97,9 @@ def calc_date():
 
     return date_dict
 
-def add_new_month(workb, sheetname, new_sheetname):
-    sheet = workb.worksheet(sheetname)
-    main_costs = workb.worksheet(title='Rendszeres kiadások')
-    cost_values = []
-    saving_values = []
 
-    new_sheet = workb.duplicate_sheet(source_sheet_id=sheet.id, insert_sheet_index=4, new_sheet_name=new_sheetname)
-
-    workb.values_clear(f"{new_sheet.title}!F4:J100")
-
-    cell = new_sheet.acell('F3', 'FORMULA')
-    n = cell.value[2:-5].split('.')
-    new_value = cell.value[:2] + n[0] + '.' + f'0{int(n[1]) + 1}' + cell.value[-5:]
-    new_sheet.update('F3', new_value, raw=False)
-
-    cost_names = main_costs.get('A2:A8')
-    saving_names = main_costs.get('G2:G6')
-
-    for i in range(len(cost_names)):
-        cell = main_costs.acell(f'B{i+2}', 'FORMULA')
-        value = cell.numeric_value
-        cost_values.append(value)
-
-    for j in range(len(saving_names)):
-        cell = main_costs.acell(f'H{j+2}', 'FORMULA')
-        value = cell.numeric_value
-        saving_values.append(value)
-
-    new_sheet.update('G4:G10', cost_names, raw=True)
-    new_sheet.update('G11:G15', saving_names, raw=True)
-    # new_sheet.update('F4:F10', [cost_values], raw=False)
-    # new_sheet.update('F11:F15', [saving_values], raw=False)
-
-    print('fdg')
-
-
-def add_new_month_v2(wbook, is_new_month=False):
+def add_new_month(wbook, creds, is_new_month=False):
     dates = calc_date()
-
     template_sheet = wbook.worksheet('Template')
 
     if is_new_month:
@@ -149,8 +114,6 @@ def add_new_month_v2(wbook, is_new_month=False):
         for i in range(13):
             new_sheet.update(f'E{i+3}', [[f"{dates['this_month']}01."]], raw=False)
 
-        edit_mounthly_costs_sheet(wbook=wbook, is_new_month=is_new_month, dates=dates)
-
     else:
         new_sheet = wbook.duplicate_sheet(source_sheet_id=template_sheet.id,
                                           insert_sheet_index=11,
@@ -163,22 +126,95 @@ def add_new_month_v2(wbook, is_new_month=False):
         for i in range(13):
             new_sheet.update(f'E{i+3}', [[f"{dates['next_month']}01."]], raw=False)
 
-        edit_mounthly_costs_sheet(wbook=wbook, is_new_month=is_new_month, dates=dates)
-        print('gd')
+    print('New sheet added succesfully!')
+
+    show_hide_wsheets(creds=creds, wbook_id=wbook.id, sheet_id=new_sheet.id, is_hidden=False)
+    edit_mounthly_costs_sheet(wbook=wbook, creds=creds, is_new_month=is_new_month, dates=dates)
 
 
-def edit_mounthly_costs_sheet(wbook,  is_new_month=False, dates=calc_date()):
-    cost_sheet = wbook.worksheet('Havi Kiadások 2021')
+def edit_mounthly_costs_sheet(wbook, creds, is_new_month=False, dates=calc_date()):
+    year = None
+
+    if is_new_month:
+        year = dates['this_month'][:-4]
+    else:
+        year = dates['next_month'][:-4]
+
+    sheet_name = f'Havi Kiadások {year}'
+    cost_sheet = wbook.worksheet(sheet_name)
     count = cost_sheet.acell('A1').numeric_value
 
-    for i in range(count + 1):
-        value = f"=SUM(SUMIF('{dates['next_month']}'!$C$4:$C$100;'Havi Kiadások 2021'!$A{i + 2};'{dates['next_month']}'!$A$4:$A$100);SUMIF('{dates['next_month']}'!$H$4:$H$100;'Havi Kiadások 2021'!$A{i + 2};'{dates['next_month']}'!$F$4:$F$100))"
-        col = None
-        key_value = dates['next_month'][-3:]
+    print(f'Modifying monthly costs table ({cost_sheet.title})  ...', end='  ')
 
-        for key in mounthly_costs_columns:
-            if key == key_value:
-                col = mounthly_costs_columns[key]
-                cost_sheet.update(f'{col}{i + 2}', [value], raw=True)
-                print('dfdf')
+    if is_new_month:
+        for i in range(count):
+            value = [f"=SUM(SUMIF('{dates['this_month']}'!$C$4:$C$100;'{sheet_name}'!$A{i + 2};'{dates['this_month']}'!$A$4:$A$100))"]
+            col = None
+            key_value = dates['this_month'][-3:]
 
+            for key in mounthly_costs_columns:
+                if key == key_value:
+                    col = mounthly_costs_columns[key]
+                    cost_sheet.update(f'{col[0]}{i + 2}', [value], raw=False)
+
+        show_hide_cols(creds=creds, wbook_id=wbook.id, sheet_id=cost_sheet.id, is_hidden=False, start=col[1]-1, end=col[1])
+
+    else:
+        for j in range(count):
+            value = [f"=SUM(SUMIF('{dates['next_month']}'!$C$4:$C$100;'{sheet_name}'!$A{j + 2};'{dates['next_month']}'!$A$4:$A$100))"]
+            col = None
+            key_value = dates['next_month'][-3:]
+
+            for key in mounthly_costs_columns:
+                if key == key_value:
+                    col = mounthly_costs_columns[key]
+                    cost_sheet.update(f'{col[0]}{j + 2}', [value], raw=False)
+
+        show_hide_cols(creds=creds, wbook_id=wbook.id, sheet_id=cost_sheet.id, is_hidden=False, start=col[1] - 1, end=col[1])
+
+    print('Done!')
+
+
+def show_hide_cols(creds, wbook_id, sheet_id, is_hidden, start, end):
+    service = discovery.build('sheets', 'v4', credentials=creds)
+
+    requests = {'updateDimensionProperties': {
+                    "range": {
+                    "sheetId": sheet_id,
+                    "dimension": 'COLUMNS',
+                    "startIndex": start,
+                    "endIndex": end},
+                "properties": {
+                    "hiddenByUser": is_hidden},
+                "fields": 'hiddenByUser'}}
+
+    body = {'requests': [requests]}
+
+    request = service.spreadsheets().batchUpdate(spreadsheetId=wbook_id, body=body)
+    response = request.execute()
+
+    return response
+
+
+def show_hide_wsheets(creds, wbook_id, sheet_id, is_hidden):
+    service = discovery.build('sheets', 'v4', credentials=creds)
+
+    requests = {'updateSheetProperties': {
+        "properties": {
+            "sheetId": sheet_id,
+            "hidden": is_hidden},
+        "fields": 'hidden'}}
+
+    body = {'requests': [requests]}
+
+    request = service.spreadsheets().batchUpdate(spreadsheetId=wbook_id, body=body)
+    response = request.execute()
+
+    return response
+
+
+if __name__ == '__main__':
+    workb, creds = open_spread_sheet('Pénz másolata')
+    date = calc_date()
+
+    edit_mounthly_costs_sheet(wbook=workb, creds=creds, is_new_month=True, dates=date)
--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 from assets import *
 
-workb = open_spread_sheet('Pénz másolata')
-# update_sheet_list(workb)
+print('~'*25, ' Welcome to Walet editor! ', '~'*25)
+print('What would you like to do?')
+print('  1 - Add new month')
+print('  2 - Add new year')
+print('  3 - Add new category')
 
-add_new_month_v2(workb, False)
-# edit_mounthly_costs_sheet(workb)
+workb, creds = open_spread_sheet('Pénz másolata')
 
+x = input('Choose a number: ')
+while x != '1' or x != '2' or x != '3':
+    x = input('Please choose form the numbers above: ')
 
-print('muhaha')
+    if x == '1':
+        value = str(input('Is today is in the new month you want to add? (y/n) '))
+        truth = None
+
+        while value != 'y' or value != 'n':
+            value = input('Please write only "y" or "n"! ')
+            if value == 'y':
+                truth = True
+                break
+            elif value == 'n':
+                truth = False
+                break
+
+        add_new_month(wbook=workb, creds=creds, is_new_month=truth)
+        break
+
+    elif x == '2':
+        print('not implemented yet')
+
+    elif x == '3':
+        print('not implemented yet')
